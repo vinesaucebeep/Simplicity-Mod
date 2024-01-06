9af95d8d792f5675fc98660f72f755372787f138
Alexander <88556555+vinesaucebeep@users.noreply.github.com>
2024-01-06 10:51:24 -0500

    Updated Main

--- src/main/java/com/beep67/simplicity/Simplicity.java
+++ src/main/java/com/beep67/simplicity/Simplicity.java
@@ -1,26 +1,14 @@
 package com.beep67.simplicity;
 
 import com.beep67.simplicity.block.BlockInit;
-import com.beep67.simplicity.block.ItemInit;
+import com.beep67.simplicity.item.ItemInit;
+import com.beep67.simplicity.item.ModCreativeTab;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(Simplicity.MODID)
@@ -31,54 +19,22 @@ public class Simplicity {
 
 
     public Simplicity() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        modEventBus.addListener(this::commonSetup);
-
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
 
-        ItemInit.ITEMS.register(modEventBus);
-        BlockInit.BLOCKS.register(modEventBus);
+        ItemInit.ITEMS.register(bus);
+        BlockInit.BLOCKS.register(bus);
+        ModCreativeTab.TABS.register(bus);
+
 
     }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(ItemInit.PINK_SALT_ROCK_ITEM);
-            event.accept(ItemInit.PINK_SALT_DUST_BLOCK);
-            event.accept(ItemInit.POLISHED_PINK_SALT_ROCK_ITEM);
-            event.accept(ItemInit.CHISELED_POLISHED_PINK_SALT_ROCK);
-            event.accept(ItemInit.POLISHED_PINK_SALT_ROCK_TILES);
-            event.accept(ItemInit.POLISHED_PINK_SALT_ROCK_BRICKS_ITEM);
-
-
-        }
-    }
-
-
 
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-    }
-
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
     }
 }
+
+
+
